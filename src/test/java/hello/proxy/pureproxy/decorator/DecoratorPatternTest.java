675aa9dfcf644ca1644a0227366fbca24d78aa6f
ttasjwi <ttasjwi920@gmail.com>
2022-09-10 16:36:13 +0900

    데코레이터 패턴 - 예제 코드3
    - Client와 realComponent 사이에, 여러 개의 Decorator가 연쇄적으로 호출되도록 했다.
    - Client는 코드를 변경하지 않았다.
    - 다른 Decorator에게 호출되는 Decorator측에서도 코드 변경이 일어나지 않았다.

--- src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -1,9 +1,6 @@
 package hello.proxy.pureproxy.decorator;
 
-import hello.proxy.pureproxy.decorator.code.Component;
-import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
-import hello.proxy.pureproxy.decorator.code.MessageDecorator;
-import hello.proxy.pureproxy.decorator.code.RealComponent;
+import hello.proxy.pureproxy.decorator.code.*;
 import org.junit.jupiter.api.Test;
 
 public class DecoratorPatternTest {
@@ -18,8 +15,17 @@ public class DecoratorPatternTest {
     @Test
     void decorator1() {
         Component realComponent = new RealComponent();
-        MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+        Component messageDecorator = new MessageDecorator(realComponent);
         DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+        client.execute();
+    }
+
+    @Test
+    void decorator2() {
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        Component timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
         client.execute();
     }
 }
