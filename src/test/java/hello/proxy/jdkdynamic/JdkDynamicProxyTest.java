d9894fd08ffb261fa4b77946410717637d609ec0
ttasjwi <ttasjwi920@gmail.com>
2022-09-13 19:57:07 +0900

    JDK 동적 프록시 - 예제 코드
    - InvocationHandler 인터페이스 구현체: 동적으로 생성할 프록시들이 공통적으로 수행하는 로직 구현
    - Proxy.newProxyInstance : 인터페이스 클래스 로더 정보, 해당 클래스가 구현해야하는 인터페이스들 정보, invocationHandler 구현체를 인자로 넘겨, 프록시를 생성
    - 프록시를 통해 메서드를 호출하면, 해당 프록시는 InvocationHandler 구현체를 통해 프록시 로직을 실행한다.
    - InvocationHandler는 target을 통해 실제 객체를 호출한다.

---
+++ src/test/java/hello/proxy/jdkdynamic/JdkDynamicProxyTest.java
@@ -0,0 +1,37 @@
+package hello.proxy.jdkdynamic;
+
+import hello.proxy.jdkdynamic.code.*;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Proxy;
+
+@Slf4j
+public class JdkDynamicProxyTest {
+
+    @Test
+    void dynamicA() {
+        AInterface target = new AImpl();
+        TimeInvocationHandler handler = new TimeInvocationHandler(target);
+
+        // 인터페이스 클래스 로더 정보, 인터페이스 정보, (구현 클래스를 통해 생성한)핸들러를 통해 프록시 생성
+        AInterface proxy = (AInterface) Proxy.newProxyInstance(AInterface.class.getClassLoader(), new Class[]{AInterface.class}, handler);
+
+        proxy.call();
+        log.info("targetClass = {}", target.getClass());
+        log.info("proxyClass = {}", proxy.getClass());
+    }
+
+    @Test
+    void dynamicB() {
+        BInterface target = new BImpl();
+        TimeInvocationHandler handler = new TimeInvocationHandler(target);
+
+        // 인터페이스 클래스 로더 정보, 인터페이스 정보, (구현 클래스를 통해 생성한)핸들러를 통해 프록시 생성
+        BInterface proxy = (BInterface) Proxy.newProxyInstance(BInterface.class.getClassLoader(), new Class[]{BInterface.class}, handler);
+
+        proxy.call();
+        log.info("targetClass = {}", target.getClass());
+        log.info("proxyClass = {}", proxy.getClass());
+    }
+}
