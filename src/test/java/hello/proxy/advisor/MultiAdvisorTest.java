3dd96e55a76eedae7a851c75cc004deb924a9b28
ttasjwi <ttasjwi920@gmail.com>
2022-09-15 19:28:24 +0900

    여러 어드바이저 적용 : 하나의 프록시, 여러 어드바이저
    - 하나의 target에 여러 어드바이저를 적용할 때는 순서대로 어드바이저를 등록하고 하나의 프록시를 생성하면 된다.
    - 실제로 스프링 AOP도 하나의 target에 하나의 프록시만 생성된다.

--- src/test/java/hello/proxy/advisor/MultiAdvisorTest.java
+++ src/test/java/hello/proxy/advisor/MultiAdvisorTest.java
@@ -35,6 +35,26 @@ public class MultiAdvisorTest {
         proxy2.save();
     }
 
+    @Test
+    @DisplayName("하나의 프록시, 여러 어드바이저")
+    void multiAdvisorTest2() {
+        // client -> proxy -> advisor2 -> advisor1 -> target
+        DefaultPointcutAdvisor advisor1 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice1());
+        DefaultPointcutAdvisor advisor2 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice2());
+
+        // 프록시 생성
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+
+        proxyFactory.addAdvisor(advisor2); // 어드바이저 등록 순서대로 수행된다.
+        proxyFactory.addAdvisor(advisor1);
+
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        //실행
+        proxy.save();
+    }
+
     @Slf4j
     static class Advice1 implements MethodInterceptor {
 
