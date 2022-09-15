e083d82d6f8d49dcc9a9ad8bce91a887b62a7b37
ttasjwi <ttasjwi920@gmail.com>
2022-09-15 20:10:06 +0900

    프록시 팩토리 - 적용2
    - 프록시 팩토리를 통해 구체 클래스 기반 프록시를 생성하여 애플리케이션에 적용하였다.

--- src/main/java/hello/proxy/config/v3_proxyfactory/advice/LogTraceAdvice.java
+++ src/main/java/hello/proxy/config/v3_proxyfactory/advice/LogTraceAdvice.java
@@ -2,10 +2,8 @@ package hello.proxy.config.v3_proxyfactory.advice;
 
 import hello.proxy.trace.TraceStatus;
 import hello.proxy.trace.logtrace.LogTrace;
-import lombok.RequiredArgsConstructor;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
-import org.springframework.util.PatternMatchUtils;
 
 import java.lang.reflect.Method;
 
